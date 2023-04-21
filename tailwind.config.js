8715cf956e67a85bc7c6a4c5c2d4757942f29231
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2023-04-20 20:16:03 -0600

    intial setup. Tailwind / Chakra

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EDF2F7',
+        secondary: '#E04C4C',
+        paragraph: '#9D9D9D',
+        title: '#0B0B0B',
+        
+      }
+
+
+    },
   },
   plugins: [],
 }
