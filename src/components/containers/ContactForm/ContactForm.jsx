769f13b6dac1903955e2875a8a7443c07f81d9a5
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2023-05-05 00:29:45 -0600

    contactform template

--- src/components/containers/ContactForm/ContactForm.jsx
+++ src/components/containers/ContactForm/ContactForm.jsx
@@ -12,6 +12,7 @@ import {
     
 } from '@chakra-ui/react';
 
+import Button from '../../Button';
 
 // !! Update Component Later
 // import Input from './Input'
@@ -21,38 +22,34 @@ import styles from './styles';
 
 const ContactForm = () => {
 
-
-
-
-
-
   return (
 <Box sx={styles.container}>
 <Text as='h1'>Contact</Text>
 <Text as='p'>What are you waiting for? Get A Quote Today</Text>
 
 <Box sx={styles.formContainer}>
-  <FormControl padding={['35']} textAlign='left'>
-<FormControl  isRequired={true} >
+  
+  <FormControl padding={['35']} textAlign='left' mb={10} >
+<FormControl  isRequired={true} padding='30px'>
     <FormLabel>First Name</FormLabel>
-    <Input type='name' borderRadius='.5rem' bg='' borderWidth='2px' padding='.75rem 1rem' outline='2px solid transparent' transitionDuration='.2s'/>
+    <Input type='name' borderRadius='.5rem' bg='' height='50px'  borderWidth='2px' padding='.95rem 3rem' outline='2px solid transparent' transitionDuration='.2s'/>
     
-<FormControl isRequired={true}> 
+<FormControl isRequired={true} mt={10}> 
     <FormLabel  >Email Address</FormLabel>
-    <Input type='email' />
+    <Input type='email' borderRadius='.5rem' bg='' borderWidth='2px'  height='50px' padding='.75rem 1rem' outline='2px solid transparent' transitionDuration='.2s'/>
     <FormHelperText>We will never share your email</FormHelperText>
     </FormControl>
 
-<FormControl>
+<FormControl mt={10} >
     <FormLabel>Phone Number</FormLabel>
-    <Input type='email'/>
+    <Input type='email' borderRadius='.5rem' bg='' borderWidth='2px' height='50px'  padding='.75rem 1rem' outline='2px solid transparent' transitionDuration='.2s'/>
     <FormHelperText>Optional</FormHelperText>
     </FormControl>
 
 
-<FormControl as='fieldset'>
-    <FormLabel as='legend' htmlFor={null}>Custom Message</FormLabel>
-    <Textarea placeholder='I would like a...' height='50px'/>
+<FormControl as='fieldset' mt={10} borderRadius='.5rem'  borderWidth='2px'  transitionDuration='.2s'>
+    <FormLabel as='legend' htmlFor={null} >Custom Message</FormLabel>
+    <Textarea placeholder='I would like a...' height='190px' bg='white'/>
  
     </FormControl>
  
@@ -66,7 +63,7 @@ const ContactForm = () => {
 
 
 </Box>
-
+<Button text='Submit'/>
 
 </Box>
       
--- src/components/containers/ContactForm/styles.js
+++ src/components/containers/ContactForm/styles.js
@@ -2,6 +2,9 @@ const styles = {
     container: {
         backgroundColor: '#E8E8F5',
         padding: ['50px'],
+        
+        
+        
 
         h1: {
             fontFamily: 'Bangers',
@@ -12,11 +15,13 @@ const styles = {
             fontSize: '18px'
         }
     },
+
     formContainer: {
-
-
-       
-    },
+        display: 'flex !important' ,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+  
 
 }
 
