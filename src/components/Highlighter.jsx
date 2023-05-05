caa2fd7873610d1b0752ec82e1df66f4f02ce168
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2023-05-05 11:31:29 -0600

    highlighter

---
+++ src/components/Highlighter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Highlighter = (props) => {
+  return (
+    <span
+    style={{
+        backgroundColor: '#E04C4C',
+        color: 'white',
+        fontFamily: 'Bangers',
+        padding: '7px 10px',
+        boxShadow: '4px 4px lightgray'
+    }}
+    >
+        {props.text}
+    </span>
+  )
+}
+
+export default Highlighter
